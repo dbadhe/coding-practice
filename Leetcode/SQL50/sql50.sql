cf166f92ed0bbadfcc604e89d6edc52cb48d30ab
Dhanashree Badhe <136118943+dbadhe@users.noreply.github.com>
2025-04-24 21:24:42 -0700

    Update sql50.sql

--- Leetcode/SQL50/sql50.sql
+++ Leetcode/SQL50/sql50.sql
@@ -3,3 +3,7 @@ select name
 from Customer 
 where referee_id  != 2 or referee_id IS NULL
 
+#595. Big Countries 
+select name, population, area
+from World 
+where area > 3000000 OR population > 25000000
