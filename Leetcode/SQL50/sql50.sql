c5abddae5cca4831ddf734a1570798bcef1b8807
Dhanashree Badhe <136118943+dbadhe@users.noreply.github.com>
2025-04-25 18:08:14 -0700

    Update sql50.sql

--- Leetcode/SQL50/sql50.sql
+++ Leetcode/SQL50/sql50.sql
@@ -7,3 +7,10 @@ where referee_id  != 2 or referee_id IS NULL
 select name, population, area
 from World 
 where area > 3000000 OR population > 25000000
+
+#1148. Article Views I 
+select distinct author_id AS id
+from Views 
+where author_id = viewer_id
+order by id ASC
+
